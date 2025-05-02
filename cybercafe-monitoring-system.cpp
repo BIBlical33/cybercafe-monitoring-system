f9ca8c63e624eb7ddd231f69fbdc41acc3424122
Илья <mag1str.kram@gmail.com>
2025-05-02 13:47:22 +0300

    Add project files.

---
+++ cybercafe-monitoring-system.cpp
@@ -0,0 +1,12 @@
+﻿// cybercafe-monitoring-system.cpp : Defines the entry point for the application.
+//
+
+#include "cybercafe-monitoring-system.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
